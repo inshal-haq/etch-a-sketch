e74a4a740fa739f4bfaa3889646f0a0a82f1996f
Inshal Haq <inshalulhaq11@gmail.com>
2023-09-06 17:13:17 -0500

    Implement change grid size button
    - Allow for user to input grid's number of squares per side and
      draw grid accordingly.
    
    - Add removeGrid() and drawGrid() to recreate grid.
    
    - Add fixed width to rowContainer CSS class. With JS, change
      square's width and height to fit within fixed row width.

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
 const container = document.querySelector('#container');
 
-// create 16x16 square grid
-for (let row = 1; row <= 16; row++) {
-  const rowContainer = document.createElement('div');
-  rowContainer.classList.add('row');
-  container.appendChild(rowContainer);
+let squaresPerSide = 16;
 
-  for (let col = 1; col <= 16; col++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    rowContainer.appendChild(box);
+// user can input squaresPerSide
+const changeGridBtn = document.querySelector('#changeGridBtn');
+changeGridBtn.addEventListener('click', () => {
+  squaresPerSide = prompt('Enter number of squares per side: ');
+  if (!squaresPerSide || squaresPerSide > 100 || squaresPerSide < 0) {
+    squaresPerSide = 16;
   }
+
+  removeGrid();
+  drawGrid();
+});
+
+drawGrid();
+
+function drawGrid() {
+  // create n x n square grid
+  for (let row = 1; row <= squaresPerSide; row++) {
+    const rowContainer = document.createElement('div');
+    rowContainer.classList.add('row');
+    container.appendChild(rowContainer);
+  
+    for (let col = 1; col <= squaresPerSide; col++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+
+      // make boxes fit in 640px row width
+      box.style.width = `${640 / squaresPerSide}px`;
+      box.style.height = `${640 / squaresPerSide}px`;
+
+      rowContainer.appendChild(box);
+    }
+  }
+
+  // on mouse enter, turn box red
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach(box => {
+    box.addEventListener('mouseenter', () => {
+      box.style.backgroundColor = 'red';
+    });
+  });
 }
 
-// on mouse enter, turn box red
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
-  box.addEventListener('mouseenter', () => {
-    box.style.backgroundColor = 'red';
-  });
-});
+function removeGrid() {
+  const rows = document.querySelectorAll('.row');
+  rows.forEach(row => row.remove());
+}
